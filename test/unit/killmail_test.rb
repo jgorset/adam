a8658f6e4c5906b10ea106b83b382d37495510c2
Johannes <jgorset@gmail.com>
2010-10-01 18:56:06 +0200

    Update tests to match changes

--- test/unit/killmail_test.rb
+++ test/unit/killmail_test.rb
@@ -20,23 +20,23 @@ class KillmailTest < Test::Unit::TestCase
     assert_equal kill.victim.pilot, "Vik Reddy"
     assert_equal kill.victim.corporation, "The Arrow Project"
     assert_equal kill.victim.alliance, "Morsus Mihi"
-    assert_equal kill.victim.faction, false
+    assert_equal kill.victim.faction, nil
     assert_equal kill.victim.ship, "Dominix"
     assert_equal kill.solar_system.name, "Y-C3EQ"
     assert_equal kill.solar_system.security_status, 0.0
     assert_equal kill.victim.damage_taken, 62329
     
-    assert_equal kill.involved_parties[0].type, "NPC"
+    assert_equal kill.involved_parties[0].type, :NPC
     assert_equal kill.involved_parties[0].ship, "Minmatar Control Tower"
     assert_equal kill.involved_parties[0].corporation, "Trux Germani"
     assert_equal kill.involved_parties[0].damage_done, 23743
     
-    assert_equal kill.involved_parties[1].type, "PC"
+    assert_equal kill.involved_parties[1].type, :PC
     assert_equal kill.involved_parties[1].pilot, "Aragorn Angelus"
     assert_equal kill.involved_parties[1].security_status, 5.0
     assert_equal kill.involved_parties[1].corporation, "Shade."
     assert_equal kill.involved_parties[1].alliance, "Cry Havoc."
-    assert_equal kill.involved_parties[1].faction, false
+    assert_equal kill.involved_parties[1].faction, nil
     assert_equal kill.involved_parties[1].ship, "Hound"
     assert_equal kill.involved_parties[1].weapon, "Caldari Navy Bane Torpedo"
     assert_equal kill.involved_parties[1].damage_done, 3835
@@ -48,7 +48,7 @@ class KillmailTest < Test::Unit::TestCase
     assert_equal kill.loot[11].name, "Antimatter Charge L"
     assert_equal kill.loot[11].dropped, false
     assert_equal kill.loot[11].quantity, 4322
-    assert_equal kill.loot[11].cargo, true
+    assert_equal kill.loot[11].cargo_hold, true
   end
   
   private
