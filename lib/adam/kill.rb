c991014bc39476693f3a4142f7b47b66de9106c8
Johannes Gorset <jgorset@gmail.com>
2010-10-01 13:03:37 +0200

    Fix a bug that caused message digest to change

--- lib/adam/kill.rb
+++ lib/adam/kill.rb
@@ -92,9 +92,9 @@ module Adam
     alias_method :to_s, :to_killmail
     
     def digest
-      string = time.to_s + victim.pilot.to_s + victim.ship.to_s + solar_system.to_s + victim.damage_taken.to_s
+      string = "#{time}#{victim.pilot}#{victim.ship}#{solar_system.name}#{victim.damage_taken}"
       involved_parties.sort! { |x, y| x.damage_done <=> y.damage_done }
-      involved_parties.each { |p| string += p.pilot.to_s + p.damage_done.to_s }
+      involved_parties.each { |p| string << "#{p.pilot}#{p.damage_done}" }
       Digest::MD5.hexdigest(string)
     end
     
