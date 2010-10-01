ad384cf42206fb22b8ea6ff9598ff509cf132e6f
Johannes <jgorset@gmail.com>
2010-10-01 19:25:41 +0200

    Change order of assert_equal arguments

--- test/unit/killmail_test.rb
+++ test/unit/killmail_test.rb
@@ -17,38 +17,38 @@ class KillmailTest < Test::Unit::TestCase
   def test_corresponding_kill_object
     kill = Adam::Killmail.parse(load_killmail('valid_killmail.txt'))
     
-    assert_equal kill.victim.pilot, "Vik Reddy"
-    assert_equal kill.victim.corporation, "The Arrow Project"
-    assert_equal kill.victim.alliance, "Morsus Mihi"
-    assert_equal kill.victim.faction, nil
-    assert_equal kill.victim.ship, "Dominix"
-    assert_equal kill.solar_system.name, "Y-C3EQ"
-    assert_equal kill.solar_system.security_status, 0.0
-    assert_equal kill.victim.damage_taken, 62329
+    assert_equal "Vik Reddy", kill.victim.pilot
+    assert_equal "The Arrow Project", kill.victim.corporation
+    assert_equal "Morsus Mihi", kill.victim.alliance
+    assert_equal nil, kill.victim.faction
+    assert_equal "Dominix", kill.victim.ship
+    assert_equal "Y-C3EQ", kill.solar_system.name
+    assert_equal 0.0, kill.solar_system.security_status
+    assert_equal 62329, kill.victim.damage_taken
     
-    assert_equal kill.involved_parties[0].type, :NPC
-    assert_equal kill.involved_parties[0].ship, "Minmatar Control Tower"
-    assert_equal kill.involved_parties[0].corporation, "Trux Germani"
-    assert_equal kill.involved_parties[0].damage_done, 23743
+    assert_equal :NPC, kill.involved_parties[0].type
+    assert_equal "Minmatar Control Tower", kill.involved_parties[0].ship
+    assert_equal "Trux Germani", kill.involved_parties[0].corporation
+    assert_equal 23743, kill.involved_parties[0].damage_done
     
-    assert_equal kill.involved_parties[1].type, :PC
-    assert_equal kill.involved_parties[1].pilot, "Aragorn Angelus"
-    assert_equal kill.involved_parties[1].security_status, 5.0
-    assert_equal kill.involved_parties[1].corporation, "Shade."
-    assert_equal kill.involved_parties[1].alliance, "Cry Havoc."
-    assert_equal kill.involved_parties[1].faction, nil
-    assert_equal kill.involved_parties[1].ship, "Hound"
-    assert_equal kill.involved_parties[1].weapon, "Caldari Navy Bane Torpedo"
-    assert_equal kill.involved_parties[1].damage_done, 3835
+    assert_equal :PC, kill.involved_parties[1].type
+    assert_equal "Aragorn Angelus", kill.involved_parties[1].pilot
+    assert_equal 5.0, kill.involved_parties[1].security_status
+    assert_equal "Shade.", kill.involved_parties[1].corporation
+    assert_equal "Cry Havoc.", kill.involved_parties[1].alliance
+    assert_equal nil, kill.involved_parties[1].faction
+    assert_equal "Hound", kill.involved_parties[1].ship
+    assert_equal "Caldari Navy Bane Torpedo", kill.involved_parties[1].weapon
+    assert_equal 3835, kill.involved_parties[1].damage_done
     
-    assert_equal kill.loot[0].name, "Antimatter Charge L"
-    assert_equal kill.loot[0].dropped, false
-    assert_equal kill.loot[0].quantity, 37
+    assert_equal "Antimatter Charge L", kill.loot[0].name
+    assert_equal false, kill.loot[0].dropped
+    assert_equal 37, kill.loot[0].quantity
     
-    assert_equal kill.loot[11].name, "Antimatter Charge L"
-    assert_equal kill.loot[11].dropped, false
-    assert_equal kill.loot[11].quantity, 4322
-    assert_equal kill.loot[11].cargo_hold, true
+    assert_equal "Antimatter Charge L", kill.loot[11].name
+    assert_equal false, kill.loot[11].dropped
+    assert_equal 4322, kill.loot[11].quantity
+    assert_equal true, kill.loot[11].cargo_hold
   end
   
   private
